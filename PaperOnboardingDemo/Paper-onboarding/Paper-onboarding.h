428cb266e1d68598e10c0ec9cde733fbe5deb3b6
Alex.k <Alex.k@ramotion.com>
2016-05-30 17:53:03 +0300

    shared scheme

---
+++ PaperOnboardingDemo/Paper-onboarding/Paper-onboarding.h
@@ -0,0 +1,19 @@
+//
+//  Paper-onboarding.h
+//  Paper-onboarding
+//
+//  Created by Alex K. on 30/05/16.
+//  Copyright © 2016 Alex K. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Paper-onboarding.
+FOUNDATION_EXPORT double Paper_onboardingVersionNumber;
+
+//! Project version string for Paper-onboarding.
+FOUNDATION_EXPORT const unsigned char Paper_onboardingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Paper_onboarding/PublicHeader.h>
+
+
