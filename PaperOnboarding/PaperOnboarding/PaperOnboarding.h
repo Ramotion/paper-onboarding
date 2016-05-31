0426b4214d23fd4f21701c0b9684131c868afbbc
Alex.k <Alex.k@ramotion.com>
2016-05-31 14:51:53 +0300

    added framework

---
+++ PaperOnboarding/PaperOnboarding/PaperOnboarding.h
@@ -0,0 +1,19 @@
+//
+//  PaperOnboarding.h
+//  PaperOnboarding
+//
+//  Created by Alex K. on 31/05/16.
+//  Copyright © 2016 Alex K. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PaperOnboarding.
+FOUNDATION_EXPORT double PaperOnboardingVersionNumber;
+
+//! Project version string for PaperOnboarding.
+FOUNDATION_EXPORT const unsigned char PaperOnboardingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PaperOnboarding/PublicHeader.h>
+
+
