6af62fbdbbc25198daad56255c26955f7ee7dff3
ChristineXYS <christine@streethawk.com>
2017-11-04 15:57:19 +0800

    Fix IOS-1199: Preview Mode does not delete local tip feed entry when dismissed Change header file name. It’s not in framework way but in folder file way instead.

--- Pointzi/SHCarouselLayout.m
+++ Pointzi/SHCarouselLayout.m
@@ -17,7 +17,7 @@
 
 #import "SHCarouselLayout.h"
 //header from StreetHawk
-#import <StreetHawkCore_Pointzi/StreetHawkCore_Pointzi.h>
+#import "StreetHawkCore_Pointzi.h"
 //header from PaperOnboarding
 #import <PaperOnboarding/PaperOnboarding-Swift.h>
 
