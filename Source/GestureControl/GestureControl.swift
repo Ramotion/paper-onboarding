c73437fccddf27352c1b0a4e29db970d7310a0d1
Mahmudul Hasan <mhasan341@gmail.com>
2022-07-08 15:17:36 +0600

    Rolling Back

--- Source/GestureControl/GestureControl.swift
+++ Source/GestureControl/GestureControl.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-protocol GestureControlDelegate: AnyObject {
+protocol GestureControlDelegate: class {
     func gestureControlDidSwipe(_ direction: UISwipeGestureRecognizer.Direction)
 }
 
